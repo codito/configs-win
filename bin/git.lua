aa51fd1345c0fbdaa66de15f24035a901831de77
Arun Mahapatra <armahapa@armahapa-srv.fareast.corp.microsoft.com>
2015-03-03 12:26:43 +0530

    Add git prompt cache.

---
+++ bin/git.lua
@@ -0,0 +1,47 @@
+-- Make sure GitPromptClient.exe, GitPromptCache.exe and git2.dll are in the same folder and also part of %path%.
+function get_repo_details()
+    for line in io.popen("GitPromptClient 2>nul"):lines() do
+        local m = line:match("^%(.+")
+        if m then
+            return m
+        end
+    end
+
+    return false
+end
+
+function git_prompt_filter()
+
+    -- Colors for git status
+    local colors = {
+        clean = "\x1b[1;37;40m",
+        dirty = "\x1b[31;1m",
+    }
+
+    local details = get_repo_details()
+
+    if details then
+
+        local branch, addedIndex, deletedIndex, modifiedIndex, addedWorkdir, deletedWorkdir, modifiedWorkdir  = string.match(details, "%((.*)%) i%[%+(%d+), %-(%d), %~(%d)%] w%[%+(%d+), %-(%d), %~(%d)%] (.*)")
+
+        local added = addedIndex + addedWorkdir
+        local deleted = deletedIndex + deletedWorkdir
+        local modified = modifiedIndex + modifiedWorkdir
+
+        local total = added + deleted + modified
+
+        if total > 0 then
+            color = colors.dirty
+        else
+            color = colors.clean
+        end
+
+        clink.prompt.value = string.gsub(clink.prompt.value, "{git}", color.."("..branch..")"..colors.clean.." [+"..added..", -"..deleted..", ~"..modified.."]")
+        return true
+    end
+    clink.prompt.value = string.gsub(clink.prompt.value, "{git}", "")
+    return false
+end
+
+clink.prompt.register_filter(git_prompt_filter, 50)
+
