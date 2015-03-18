e0d0f7d6da20549e84f89205dfcd76d610cb9202
Arun Mahapatra <pratikarun@gmail.com>
2015-03-18 21:21:15 +0530

    Add a setup-machine script install a set of chocolatey packages!

---
+++ bin/Setup-Machine.ps1
@@ -0,0 +1,16 @@
+choco install vim
+choco install clover
+choco install conemu
+choco install sysinternals
+choco install 7zip
+choco install foobar2000
+choco install f.lux
+choco install git
+choco install gittfs
+choco install google-chrome-x64
+
+choco install python -x86
+
+choco install stylecop
+choco install ilspy
+choco install resharper
