3e9115add4577af2a420584663027d4ba2d15fa6
codito <pratikarun@gmail.com>
2015-12-06 13:40:37 +0000

    Remove clover from default packages. Use python3 package instead of python. Add steps for vim setup to setup-machine. Update vimfiles to latest.

--- bin/Setup-Machine.ps1
+++ bin/Setup-Machine.ps1
@@ -1,5 +1,7 @@
+# Install chocolatey packages
+#
+Write-Host "Setup: Installing choco packages..."
 choco install -y vim
-choco install -y clover
 choco install -y conemu
 choco install -y sysinternals
 choco install -y 7zip
@@ -9,8 +11,23 @@ choco install -y git
 choco install -y gittfs
 choco install -y google-chrome-x64
 
-choco install -y python-x86_32
+choco install -y python3-x86_32
 
 choco install -y stylecop
 choco install -y ilspy
 choco install -y resharper
+choco install -y insomnia
+Write-Host "Setup: Installing choco packages: Complete."
+
+# Setup vim
+#
+cd ~
+git init
+git remote add origin https://github.com/codito/configs-win.git
+git fetch origin
+git checkout -t origin/master
+git submodule update --init
+
+cmd /c mklink %userprofile%\_vimrc %userprofile%\vimfiles\vimrc
+cmd /c mklink %userprofile%\_gvimrc %userprofile%\vimfiles\gvimrc
+mkdir d:\backups
