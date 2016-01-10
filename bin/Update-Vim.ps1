479476bf8e64fae116ba6f861134baf6348e39f3
codito <pratikarun@gmail.com>
2016-01-10 04:56:28 +0000

    Automate vim upgrade to daily builds.

---
+++ bin/Update-Vim.ps1
@@ -0,0 +1,14 @@
+$vimDownloadPath = Join-Path $env:temp "complete-x86.7z"
+
+if (Test-Path $vimDownloadPath) {
+    Write-Output "Found previous downloaded vim at '$vimDownloadPath'. Deleting it."
+    Remove-Item -Force $vimDownloadPath
+}
+
+Write-Output "Downloading latest vim package..."
+(New-Object System.Net.WebClient).DownloadFile("http://tuxproject.de/projects/vim/complete-x86.7z", "$env:temp\complete-x86.7z")
+
+Write-Output "Extract package..."
+& "$env:SystemDrive\Program Files\7-Zip\7z.exe" x "$env:temp\complete-x86.7z" -o"$env:SystemDrive\Program Files (x86)\vim\vim74\" -aoa
+
+Write-Output "We're done."
