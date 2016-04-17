f53650283a1b8017f26d2a1a908f2876ece6f832
codito <pratikarun@gmail.com>
2016-04-17 16:07:20 +0000

    Partial move to scoop.sh.

--- bin/Setup-Machine.ps1
+++ bin/Setup-Machine.ps1
@@ -1,14 +1,28 @@
+# Install scoop packages
+#
+iex (new-object net.webclient).downloadstring('https://get.scoop.sh')
+
+scoop install gvim
+scoop install conemu
+scoop install sysinternals
+scoop install 7zip
+#scoop install foobar2000
+#scoop install f.lux
+scoop install git
+#scoop install google-chrome-x64
+scoop install python
+
 # Install chocolatey packages
 #
 Write-Host "Setup: Installing choco packages..."
-choco install -y vim
-choco install -y conemu
-choco install -y sysinternals
-choco install -y 7zip
+#choco install -y vim
+#choco install -y conemu
+#choco install -y sysinternals
+#choco install -y 7zip
 choco install -y foobar2000
 choco install -y f.lux
-choco install -y git
-choco install -y gittfs
+#choco install -y git
+#choco install -y gittfs
 choco install -y google-chrome-x64
 
 choco install -y python3-x86_32
