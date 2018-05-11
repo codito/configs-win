6b3f9837eafeb1afc9fc70acacf924a0c1d4fabe
Arun Mahapatra <codito@users.noreply.github.com>
2018-05-11 14:24:14 +0530

    Disable choco packages

--- bin/Setup-Machine.ps1
+++ bin/Setup-Machine.ps1
@@ -3,35 +3,32 @@
 scoop bucket add extras
 
 scoop install gvim
-scoop install conemu
+#scoop install conemu
 scoop install sysinternals
 scoop install 7zip
-#scoop install foobar2000
-#scoop install f.lux
 scoop install git
-#scoop install google-chrome-x64
-scoop install python
+scoop install python -a 32bit
 
 # Install chocolatey packages
 #
-Write-Host "Setup: Installing choco packages..."
+#Write-Host "Setup: Installing choco packages..."
 #choco install -y vim
 #choco install -y conemu
 #choco install -y sysinternals
 #choco install -y 7zip
-choco install -y foobar2000
-choco install -y f.lux
+#choco install -y foobar2000
+#choco install -y f.lux
 #choco install -y git
 #choco install -y gittfs
-choco install -y google-chrome-x64
+#choco install -y google-chrome-x64
 
-choco install -y python3-x86_32
+#choco install -y python3-x86_32
 
-choco install -y stylecop
-choco install -y ilspy
-choco install -y resharper
-choco install -y insomnia
-Write-Host "Setup: Installing choco packages: Complete."
+#choco install -y stylecop
+#choco install -y ilspy
+#choco install -y resharper
+#choco install -y insomnia
+#Write-Host "Setup: Installing choco packages: Complete."
 
 # Fetch windows configuration
 #
