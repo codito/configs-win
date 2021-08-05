6a88690fca3f59726b6d66cebc47102ff23ef388
Arun Mahapatra <codito@codito.in>
2021-08-05 20:56:55 +0530

    fix: vsvars32 for VS 2022 preview.

--- bin/vsvars32.ps1
+++ bin/vsvars32.ps1
@@ -1,28 +1,13 @@
 # Visual Studio powershell helper script
-# Based on http://www.tavaresstudios.com/Blog/post/The-last-vsvars32ps1-Ill-ever-need.aspx
-
-function Get-Batchfile ($file)
-{
-    $cmd = "`"$file`" & set"
-    cmd /c $cmd | Foreach-Object {
-        if ($_.Contains('=')) {
-            $p, $v = $_.split('=')
-            Set-Item -path env:$p -value $v
-        }
-    }
-}
+# Based on https://intellitect.com/enter-vsdevshell-powershell/
 
 function VsVars32($version)
 {
-    $key = "HKLM:SOFTWARE\Wow6432Node\Microsoft\VisualStudio\" + $version
-    $VsKey = get-ItemProperty $key
-    $VsInstallPath = [System.IO.Path]::GetDirectoryName($VsKey.InstallDir)
-    $VsToolsDir = [System.IO.Path]::GetDirectoryName($VsInstallPath)
-    $VsToolsDir = [System.IO.Path]::Combine($VsToolsDir, "Tools")
-    $BatchFile = [System.IO.Path]::Combine($VsToolsDir, "vsvars32.bat")
-    Get-Batchfile $BatchFile
+    $installPath = &"${env:ProgramFiles(x86)}\Microsoft Visual Studio\Installer\vswhere.exe" -version "$version" -prerelease -property installationpath
+    Import-Module (Join-Path $installPath "Common7\Tools\Microsoft.VisualStudio.DevShell.dll")
+    Enter-VsDevShell -VsInstallPath $installPath -SkipAutomaticLocation
     [System.Console]::Title = "Visual Studio " + $version + " Windows Powershell"
 }
 
-VsVars32("14.0")
+VsVars32("17.0")
 # vim: set ft=powershell:
