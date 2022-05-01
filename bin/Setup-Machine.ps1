2096c4af7bd9dda5a26058cb188a7883ebf83c4e
Arun Mahapatra <codito@codito.in>
2022-05-01 10:09:10 +0530

    chore: update machine setup.

--- bin/Setup-Machine.ps1
+++ bin/Setup-Machine.ps1
@@ -2,19 +2,9 @@
 #
 scoop install git
 scoop bucket add extras
+scoop bucket add versions
 scoop bucket add dodorz https://github.com/dodorz/scoop.git
 scoop bucket add nerd-fonts https://github.com/matthewjberger/scoop-nerd-fonts
-
-scoop install sudo
-scoop install 7zip
-scoop install ag
-scoop install autojump-rs
-scoop install conemu
-sudo scoop install jetbrains-mono jetbrainsmono-nf
-scoop install python
-scoop install starship
-scoop install sysinternals
-scoop install vim-tux
 
 # Fetch windows configuration
 #
@@ -25,6 +15,12 @@ git fetch origin
 git checkout -t origin/master
 git submodule init
 git submodule update
+
+# Install apps
+#
+cat ~\.config\scoop\apps.txt | % {
+    scoop install $_
+}
 
 # Setup vim
 #
